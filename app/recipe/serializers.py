f3dd12e55cc96498a7789e7b68931cbe921dfeda
Max Morris <morrm046@gmail.com>
2024-02-04 23:33:13 +0000

    Add RecipeDetail Serializer
    Update Recipe viewset to use Recipe Detail Serializer for get serializer class
    
    Add tests for Recipe
     - create recipe
     - partial update recipe
     - full update recipe
     - delete recipe
     - delete recipe from other user

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 """
-Serializers for Recipe API
+Serializers for recipe APIs
 """
 
 from rest_framework import serializers
@@ -8,9 +8,16 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializers for Recipe"""
+    """Serializer for recipes."""
 
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'time_minutes', 'price', 'link')
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,9 +15,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
